a8e564ef8f3da59a80555e8971dce2e45e6c6785
dianfitrip <dianfitri683@gmail.com>
2024-05-11 12:30:21 +0700

    looping di bagaian peek part 2

--- STACK/STACK.cpp
+++ STACK/STACK.cpp
@@ -60,4 +60,13 @@ public:
 		}
 		else {
 			Node* current = top;
-			
+			while (current != NULL)
+			{
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}// return the value of the top node
+	}
+
+	
